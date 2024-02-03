4b0aff5af0df1faf08eead0d0c4130a28b9235f6
imran2024bd <imranitbd34@gmail.com>
2024-02-03 12:08:29 +0600

    js-function-practice-tasks start

--- task.js
+++ task.js
@@ -111,3 +111,32 @@ console.log(evens);
 // }
 // evenSizedString('Dhaka');
 // evenSizedString('faka');
+
+//**************************************************************************************
+// sum of the even numbers
+
+// function evenNumbersOnly(numbers) {
+//     // console.log(numbers);
+//     const evens = [];
+//     for(const number of numbers){
+//         if( number % 2 === 0){
+//             console.log(number);
+//             evens.push(number)
+//         }
+//     }
+//     return evens;
+// }
+const numbers = [5, 8, 91, 24, 6];
+
+function sumOfEvenNumbers(numbers) {
+    let sum = 0;
+    for ( const number of numbers){
+       if (number % 2 ===0){
+        console.log(number);
+        sum = sum + number;
+       }
+    }
+    return sum;   
+}
+const sum = sumOfEvenNumbers(numbers);
+console.log( 'sum of the even numbers is' ,  sum);

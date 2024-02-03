8f2dba909a934da5995f127df2d3f9c582394aa4
imran2024bd <imranitbd34@gmail.com>
2024-02-03 18:21:39 +0600

    js function object start

---
+++ function-object.js
@@ -0,0 +1,41 @@
+// function hellow() {
+//     console.log('Hellow');
+// }
+// hellow();
+
+// function hellow() {
+//     return 'hello';
+// }
+// console.log(hellow());
+
+// function hellow() {
+//     console.log('before return');
+//     return 'hello';
+//     console.log('before return');
+// }
+// console.log(hellow());
+
+
+// function hellow(myname) {
+    
+//     // return 'hello' + ' ' + myname;
+//     // alternative way
+//     return 'hello' , ${myname} 'Hossain';
+    
+// }
+// console.log(hellow("imran"));
+// console.log(hellow("Nafisa"));
+// console.log(hellow("Iffa"));
+
+const course ={
+    subbject : "javascript",
+    duration : '2 month',
+    credit : 1.5
+}
+
+
+function coursedetails(obj) {
+
+    console.log(obj);
+}
+coursedetails( course);
--- function.js
+++ function.js
@@ -228,17 +228,19 @@
 //     }
 //     return evens;
 // }
-const numbers = [5, 8, 91, 24, 6];
+// const numbers = [5, 8, 91, 24, 6];
 
-function sumOfEvenNumbers(numbers) {
-    let sum = 0;
-    for ( const number of numbers){
-       if (number % 2 ===0){
-        console.log(number);
-        sum = sum + number;
-       }
-    }
-    return sum;   
-}
-const sum = sumOfEvenNumbers(numbers);
-console.log( 'sum of the even numbers is' ,  sum);
+// function sumOfEvenNumbers(numbers) {
+//     let sum = 0;
+//     for ( const number of numbers){
+//        if (number % 2 ===0){
+//         console.log(number);
+//         sum = sum + number;
+//        }
+//     }
+//     return sum;   
+// }
+// const sum = sumOfEvenNumbers(numbers);
+// console.log( 'sum of the even numbers is' ,  sum);
+
+

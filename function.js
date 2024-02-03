d428790623674695928df94bd6f0e926876cf992
imran2024bd <imranitbd34@gmail.com>
2024-02-03 11:56:27 +0600

    fundamental of functions learning is done

--- function.js
+++ function.js
@@ -150,16 +150,95 @@
 // console.log(doubleOrTriple(5 , false));
 
 // array
-function numberOfElements(numbers){
-    const len = numbers.length;
-    return len;
+// function numberOfElements(numbers){
+//     const len = numbers.length;
+//     return len;
+// }
+
+// numberOfElements([12, 45, 78,45,121254,4,5])
+
+// // *************************************************************
+
+// function getAge(person){
+//     const age = person.age;
+//     return age;
+// }
+
+
+// **************        20-7 Sum Of All Numbers In An Array Using Function
+
+/**
+ * Objective: write a function to give me the sum of all numbers in an array
+ * step-1: declare a function
+ * step-2: call check whether the function  is called properly
+ * step-3: set a parameter(s)
+ * step-4: pas the parameter(s), check whether parameter is passed in a proper format
+ * step-5: do the function tasks (step by step)
+*/
+
+// function sumOfNumbers(numbers) {
+//     let sum = 0;
+//     for (const number of numbers ){
+//         console.log(number);
+//         sum = sum + number;
+//     } 
+//     return sum ;
+// }
+// const numbs = [54, 62, 12, 6];
+// const sum = sumOfNumbers(numbs);
+// console.log('sum of numbers is' , sum);
+
+
+
+//***********20-8 Return All The Even Numbers Of An Array */
+
+/**
+ * create function that will return only the even numbers
+ * return the sum of even numbers
+*/
+
+// function evenNumbersOnly(numbers) {
+//     // console.log(numbers);
+//     const evens = [];
+//     for(const number of numbers){
+//         if( number % 2 === 0){
+//             console.log(number);
+//             evens.push(number)
+//         }
+//     }
+//     return evens;
+// }
+// const numbers = [5, 8, 91, 24, 6];
+// const evens = evenNumbersOnly(numbers);
+// console.log(evens);
+
+
+
+//**************************************************************************************
+// sum of the even numbers
+
+// function evenNumbersOnly(numbers) {
+//     // console.log(numbers);
+//     const evens = [];
+//     for(const number of numbers){
+//         if( number % 2 === 0){
+//             console.log(number);
+//             evens.push(number)
+//         }
+//     }
+//     return evens;
+// }
+const numbers = [5, 8, 91, 24, 6];
+
+function sumOfEvenNumbers(numbers) {
+    let sum = 0;
+    for ( const number of numbers){
+       if (number % 2 ===0){
+        console.log(number);
+        sum = sum + number;
+       }
+    }
+    return sum;   
 }
-
-numberOfElements([12, 45, 78,45,121254,4,5])
-
-// *************************************************************
-
-function getAge(person){
-    const age = person.age;
-    return age;
-}
+const sum = sumOfEvenNumbers(numbers);
+console.log( 'sum of the even numbers is' ,  sum);
--- task.js
+++ task.js
@@ -49,3 +49,65 @@ function difference(num1 , num2) {
 const fatherAge = 57 ;
 const myAge = 38;
 difference(fatherAge , myAge);
+
+**
+ * create function that will return only the even numbers
+ * return the sum of even numbers
+*/
+
+
+function evenNumbersOnly(numbers) {
+    // console.log(numbers);
+    const evens = [];
+    for(const number of numbers){
+        if( number % 2 === 0){
+            console.log(number);
+            evens.push(number)
+        }
+    }
+    return evens;
+}
+const numbers = [5, 8, 91, 24, 6];
+const evens = evenNumbersOnly(numbers);
+console.log(evens);
+
+
+/**
+ * Objective: write a function to give me the sum of all numbers in an array
+ * step-1: declare a function
+ * step-2: call check whether the function  is called properly
+ * step-3: set a parameter(s)
+ * step-4: pas the parameter(s), check whether parameter is passed in a proper format
+ * step-5: do the function tasks (step by step)
+*/
+
+// function sumOfNumbers(numbers) {
+//     let sum = 0;
+//     for (const number of numbers ){
+//         console.log(number);
+//         sum = sum + number;
+//     } 
+//     return sum ;
+// }
+// const numbs = [54, 62, 12, 6];
+// const sum = sumOfNumbers(numbs);
+// console.log('sum of numbers is' , sum);
+
+
+/**
+ * for a given string tell me whether it has even number of characters or not
+ * 
+*/
+// function evenSizedString(str) {
+//     const size = str.length;
+//     console.log(str , size);
+//     if (size % 2 === 0)  {
+//         console.log('even size');
+//     }
+//     else{
+//         console.log('Odd size');
+//     }
+    
+// }
+// evenSizedString('Dhaka');
+// evenSizedString('faka');
